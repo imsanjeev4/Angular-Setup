34792640b0c5e6e8744df32f03e92ae84a340d03
imsanjeev4 <sanjeev.gupta@in.hanusoftware.com>
2017-02-15 10:33:31 +0530

    #1 Added Angular setup file

---
+++ angular2teststart/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h2>Hi From Angular2! Today is Belantine Day</h2>'
+})
+export class AppComponent {
+
+}
